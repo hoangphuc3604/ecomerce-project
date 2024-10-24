fdac0a7cc0114222271340c9c620469ab2995441
22120278 <nvhphuc364@gmail.com>
2024-10-24 23:26:23 +0700

    detach Search component

--- dashboard/src/views/components/Search.jsx
+++ dashboard/src/views/components/Search.jsx
@@ -15,7 +15,7 @@ const Search = ({ setParPage, searchValue, setSearchValue }) => {
                 value={searchValue}/> */}
 
                 <div className="flex items-center justify-center p-5">
-                    <div className="rounded-lg bg-gray-200 p-5">
+                    <div className="rounded-lg">
                         <div className="flex">
                         <div className="flex w-10 items-center justify-center rounded-tl-lg rounded-bl-lg border-r border-gray-200 bg-white p-5">
                             <svg
@@ -37,7 +37,7 @@ const Search = ({ setParPage, searchValue, setSearchValue }) => {
                         <input
                             type="button"
                             value="Search"
-                            className="bg-blue-500 p-2 rounded-tr-lg rounded-br-lg text-white font-semibold hover:bg-blue-800 transition-colors"
+                            className="bg-green-500 p-2 rounded-tr-lg rounded-br-lg text-white font-semibold hover:bg-green-800 transition-colors cursor-pointer"
                         />
                         </div>
                     </div>
